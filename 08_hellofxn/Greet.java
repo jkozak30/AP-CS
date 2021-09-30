5d33b5b863ca8e03d59cf17c4606068f67afcb25
Julia Kozak <jkozak30@stuy.edu>
2021-09-30 16:46:08 -0400

    added Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+public static void main(String[] args){
+greet("Alice");
+greet("Bob");
+greet("Charlie");
+}
+public static void greet(String s){
+System.out.println("Why, hello there, " + s + ". How do you do?");
+}
+}
