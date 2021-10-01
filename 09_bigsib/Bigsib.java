df16c285f9dcb384e5bc6c1c68c3e0e8659c31c0
Julia Kozak <jkozak30@stuy.edu>
2021-10-01 18:19:00 -0400

    updated programs

---
+++ 09_bigsib/Bigsib.java
@@ -0,0 +1,11 @@
+//Julia Kozak
+//APCS
+//HW09 -- What are BigSibs Good For?
+//2021-10-04
+
+public class Bigsib {
+	public static void greet(String s) {
+		System.out.println("Why, hello there, " + s + ". How do you do?");
+	}
+}
+
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,12 @@
+//Julia Kozak
+//APCS
+//HW09 -- What are BigSibs Good For?
+//2021-10-04
+
+public class Greet {
+    public static void main( String[] args ) {
+        Bigsib.greet("Flim");
+        Bigsib.greet("Flam");
+        Bigsib.greet("Kazaam");
+    }
+}
