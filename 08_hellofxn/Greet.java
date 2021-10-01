ce91606fb88f0e02334175d7f8e97151dcd4c76d
Julia Kozak <jkozak30@stuy.edu>
2021-10-01 18:22:10 -0400

    formatting and heading

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,10 +1,15 @@
+//Julia Kozak
+//APCS
+//HW08 -- Refactor Freshie Zero
+//2021-10-01
+
 public class Greet {
-public static void main(String[] args){
-greet("Alice");
-greet("Bob");
-greet("Charlie");
-}
+	public static void main(String[] args){
+		greet("Alice");
+		greet("Bob");
+		greet("Charlie");
+	}
 public static void greet(String s){
-System.out.println("Why, hello there, " + s + ". How do you do?");
-}
+	System.out.println("Why, hello there, " + s + ". How do you do?");
+	}
 }
