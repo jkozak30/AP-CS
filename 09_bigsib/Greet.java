794fbc12e1df29dd173613ae54a068fc54caccb2
Julia Kozak <jkozak30@stuy.edu>
2021-10-01 18:36:31 -0400

    block comment

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -3,6 +3,10 @@
 //HW09 -- What are BigSibs Good For?
 //2021-10-04
 
+/* DISCOVERIES: you can access a method from a different class using <class>.<method>();
+UNRESOLVED QUESTIONS: 
+*/
+
 public class Greet {
     public static void main( String[] args ) {
         Bigsib.greet("Flim");
