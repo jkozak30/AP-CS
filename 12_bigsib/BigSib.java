825a0bb4ff66c40a94dcdb99d98558089b0f485f
Julia Kozak <90654842+jkozak30@users.noreply.github.com>
2021-10-05 19:38:18 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,1 +1,22 @@
+//Julia Kozak
+//APCS
+//HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+//2021-10-06
+
+/* DISCO: "static" won't work with instance variables;
+QCC:
+*/
+
+public class BigSib {
+	String helloMsg;
+	public String setHelloMsg(String s) {
+		helloMsg = s;
+		return helloMsg;
+	}
+	public String greet(String s) {
+		String greeting = helloMsg;
+		String message = (greeting + " " + s);
+		return message;
+	}
+}
 
