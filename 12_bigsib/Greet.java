9ee45e88bf3c56372da5957df1200453721fe4c4
Julia Kozak <90654842+jkozak30@users.noreply.github.com>
2021-10-05 19:38:38 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,1 +1,21 @@
+//Julia Kozak
+//APCS
+//HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+//2021-10-06
+
+/* DISCOVERIES:
+UNRESOLVED QUESTIONS:
+*/
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
 
