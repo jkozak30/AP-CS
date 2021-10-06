abe194270dbdce36a75ab1ddf04b215a8da921cc
Julia Kozak <90654842+jkozak30@users.noreply.github.com>
2021-10-06 18:00:44 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,1 +1,21 @@
+//Julia Kozak
+//APCS
+//HW13 -- Where do BigSibs Come From?
+//2021-10-07
 
+/* DISCO: "static" won't work with instance variables;
+QCC:
+*/
+
+public class BigSib {
+	String helloMsg;
+	public String setHelloMsg(String s) {
+		helloMsg = s;
+		return helloMsg;
+	}
+	public String greet(String s) {
+		String greeting = helloMsg;
+		String message = (greeting + " " + s);
+		return message;
+	}
+}
