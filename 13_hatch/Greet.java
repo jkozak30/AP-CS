7a9a764993f31b1a108801914a19149d5bf2d393
Julia Kozak <90654842+jkozak30@users.noreply.github.com>
2021-10-06 17:57:45 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,7 +1,7 @@
 //Julia Kozak
 //APCS
-//HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
-//2021-10-06
+//HW13 -- Where do BigSibs Come From?
+//2021-10-07
 
 /* DISCOVERIES:
 UNRESOLVED QUESTIONS:
