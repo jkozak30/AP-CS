17e1a7074cd47aa8e59e11c80cd7957f1618893e
Julia Kozak <jkozak30@stuy.edu>
2021-11-08 20:43:05 -0500

    pig

--- lab00_oink/v1/Pig.java
+++ lab00_oink/v1/Pig.java
@@ -1,10 +1,13 @@
 /***
- * Julia Kozak, Rin Fukuoka
+ * Team Won: Julia Kozak, Rin Fukuoka
  * APCS
  * L00 -- Etterbay Odingcay Oughthray Ollaborationcay
  * 2021-11-10
  * time spent: .5hrs
  *
+ * DISCO: You can think about spaces as punctuation;
+ *        toUpperCase and toLowerCase can be used;
+ * QCC: How do we incorporate the Scanner functionality?
  * class Pig
  * a Pig Latin translator
  *           9
@@ -24,7 +27,7 @@ public class Pig {
   //static vars
   private static final String VOWELS = "aeiou";
   private static final String CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  private static final String PUNCS = ".,:;!? ";
+  private static final String PUNCS = ".,:;!? 0123456789";
 
   //checks for specific letter
   public static boolean hasA( String w, String letter ) {
@@ -97,14 +100,16 @@ public class Pig {
     String ans = "";
     if (hasPunc(w)) {
       for (int i = 0; i < w.length(); i++) {
-        if (isPunc(w.substring(i, i+1)) && i+1 < w.length()) {
+        if (isPunc(w.substring(i, i+1)) && i+1 < w.length() && i != 0) {
           return engToPig(w.substring(0, i)) + w.substring(i, i+1) + engToPig(w.substring(i+1));
         } else if (isPunc(w.substring(i, i+1)) && i+1 == w.length()){
           return engToPig(w.substring(0, i)) + w.substring(i, i+1);
-        }
+        } else if (isPunc(w.substring(i, i+1)) && i == 0) {
+	  return w.substring(0, 1) + engToPig(w.substring(1));
+	}
       }
     }
-    if ( beginsWithVowel(w) ) {
+    if ( beginsWithVowel(w.toLowerCase()) ) {
       ans = w + "way";
     } else {
       int vPos = w.indexOf( firstVowel(w) );
@@ -121,7 +126,7 @@ public class Pig {
 
   public static void main( String[] args ) {
     System.out.println(engToPig("Wow"));
-    System.out.println(engToPig("A,asjnjs ks"));
+    System.out.println(engToPig("A12s"));
     /*
     for( String word : args ) {
       System.out.println( "allVowels \t" + allVowels(word) );
