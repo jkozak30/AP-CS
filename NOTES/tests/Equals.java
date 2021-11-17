466df2225132748e5490ddb9331e4d4c24dfc543
Julia Kozak <jkozak30@stuy.edu>
2021-11-17 13:24:39 -0500

    start

---
+++ NOTES/tests/Equals.java
@@ -0,0 +1,8 @@
+public class Equals {
+	public static Integer x = new Integer(3);
+	public static Integer y = 3;
+	public static void main(String[] args ) {
+		System.out.println(x==y);
+		System.out.println(x.equals(y));
+	}
+}
