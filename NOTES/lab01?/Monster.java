958b8d21049ffad0597e913052ccc72e02031948
Julia Kozak <jkozak30@stuy.edu>
2021-11-22 22:18:32 -0500

    lab attempt

---
+++ NOTES/lab01?/Monster.java
@@ -0,0 +1,31 @@
+/*
+  class Monster -- Represents random incarnations of
+  the adventurer's natural enemy in Ye Olde RPG
+**/
+
+public class Monster extends Character{
+
+  // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  /**
+     default constructor
+     pre:  instance vars are declared
+     post: initializes instance vars.
+  **/
+  public Monster() {
+    health = 150;
+    strength = 20 + (int)( Math.random() * 45 ); // [20,65)
+    defense = 20;
+    attack = 1;
+  }
+
+
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+}//end class Monster
