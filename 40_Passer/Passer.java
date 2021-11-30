be1d58fb4177ea89af0ba19003cce87e9a7292fd
Julia Kozak <jkozak30@stuy.edu>
2021-11-30 14:03:21 -0500

    boolean

--- 40_Passer/Passer.java
+++ 40_Passer/Passer.java
@@ -9,6 +9,15 @@ public class Passer {
 		return x;
 	}
 	
+	public static boolean varChange(boolean x) {
+		if (x) {
+			x = false;
+		} else {
+			x = true;
+		}
+		return x;
+	}
+	
 	public static void main(String[] args) {
 		int a = 4;
 		System.out.println("value of a: " + a);
@@ -20,12 +29,26 @@ public class Passer {
 		} else {
 			System.out.println("java is pass-by-reference.");
 		}
+		System.out.println();
+		
 		String b = "hello";
 		System.out.println("value of b: " + b);
 		String checkB = b;
 		System.out.println("output of varChange: " + varChange(b));
-		System.out.println("new value of a: " + b);
+		System.out.println("new value of b: " + b);
 		if (checkB.equals(b)) {
+			System.out.println("java is pass-by-value.");
+		} else {
+			System.out.println("java is pass-by-reference.");
+		}
+		System.out.println();
+		
+		boolean c = false;
+		System.out.println("value of c: " + c);
+		boolean checkC = c;
+		System.out.println("output of varChange: " + varChange(c));
+		System.out.println("new value of c: " + c);
+		if ((c && checkC) || (!c && !checkC)) {
 			System.out.println("java is pass-by-value.");
 		} else {
 			System.out.println("java is pass-by-reference.");
