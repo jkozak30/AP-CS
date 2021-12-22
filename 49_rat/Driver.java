11c4cfc0ebc9ef2af9b3418c2d1a7b2f8c58aa8f
Julia Kozak <jkozak30@stuy.edu>
2021-12-22 17:58:00 -0500

    heading

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,3 +1,17 @@
+/*
+Team Orange Marshmellows: Julia Kozak, Nina Jiang, Diana Akhmedova // Duckies: Flopsy, Miss Alpaca, Ajax
+APCS pd08
+HW49 -- Rational Standards Compliance
+2021-12-23
+Time Spent: 0.5 hrs
+*/
+
+/*
+DISCO: If an object uses the constructor of a certain class type, it can be typecasted as that class type.
+       Rational implementing Comparable means that Rational is a type of Comparable.
+QCC:   If asking to run .equals() with a non-Rational object throws an error, is there a way to have the proceeding calls in the main method still run?
+*/
+
 public class Driver {
 	public static void main(String[] args) {
 		Object a = new Rational(1, 2);
