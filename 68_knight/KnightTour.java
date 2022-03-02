9f935028aa20e9403b7707d00e52cb1dcd19fb67
Julia Kozak <jkozak30@stuy.edu>
2022-03-01 20:40:24 -0500

    7

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -1,8 +1,8 @@
-// Clyde Sinclair
-// APCS pd0
+// Great, Nice Jingles: Julia Kozak, Gloria Lee, Nafiz Labib (Flopsy, Flounder, Martha)
+// APCS pd08
 // HW68 -- recursively probing for a closed cycle
 // 2022-02-28m
-// time spent:  hrs
+// time spent:  0.5hrs (except for ... time trials)
 
 /***
  * SKELETON
@@ -14,17 +14,24 @@
  * $ java KnightTour
  * $ java KnightTour [N]
  *
- * ALGO
+ * ALGO: Set the default board to one filled with 0s that is of your desired size, but surrounded by a border of -1s that is 2 wide.
+ *       Start with move 1, and set your first square to the starting square of value 1. For each of the 8 possible moves, check if the landing square is of value 0 (meaning it's a potential move). If so, move there and repeat the steps for that square, incrementing your move number by 1.
+ *       If at any point you have no potential moves, reset the current square's value to 0 and try the next possible move on the same branch.
+ *       Once your move counter reaches an integer beyond the number of potential moves (N^2 + 1), you know every move has been made, so set _solved to true (the board has been solved).
  *
- * DISCO
+ * DISCO: ANSI control code can clear the screen, change cursor position, etc.
+ *        It's fine to have your several classes in one file.
+ *        "time" can be used in the command line to record the runtime of your program.
  *
- * QCC
+ * QCC:   Is there a way to track time using currentTimeMillis() in this case?
+ *        Is it more optimal to have the same sequence of tested moves for each branch, or should it be randomized/based on the previous move in any way to improve efficiency?
+ *        Instead of using System.exit() and "return," is there a way to do this with a non-void method that returns a boolean (like with n Queens)?
  *
- * Mean execution times for boards of size n*n:
- * n=5   __s    across __ executions
- * n=6   __s    across __ executions
- * n=7   __s    across __ executions
- * n=8   __s    across __ executions
+ * Mean execution times for boards of size n*n (random starting sq):
+ * n=5   7.77s    across 10 executions
+ * n=6   47.23s    across 3 executions (13.74, 12.88, 115.07)
+ * n=7   1931.13s    across 1 executions ( -_- )
+ * n=8   654.92s    across 1 executions
  *
  * POSIX PROTIP: to measure execution time from BASH, use time program:
  * $ time java KnightTour 5
@@ -40,6 +47,7 @@ public class KnightTour
 {
   public static void main( String[] args )
   {
+    //System.out.println("why no print");
     int n = 8;
 
     try {
@@ -52,20 +60,28 @@ public class KnightTour
     TourFinder tf = new TourFinder( n );
 
     //clear screen using ANSI control code
-    System.out.println( "[2J" );
+    System.out.println( "" );
 
     //display board
     System.out.println( tf );
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for fixed starting location, use line below:
+    //System.out.println("\033c");
     tf.findTour( 2, 2, 1 );
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for random starting location, use lines below:
-    //int startX = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
-    //int startY = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
+
+    /*for (int i=0; i<1; i++) {
+      int startX = (int)(Math.random() * n) + 2;
+      int startY = (int)(Math.random() * n) + 2;
+      tf.findTour( startX, startY, 1 );
+    }*/
+
+    //int startX = (int)(Math.random() * n) + 2;
+    //int startY = (int)(Math.random() * n) + 2;
     //tf.findTour( startX, startY, 1 );   // 1 or 0 ?
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -85,6 +101,10 @@ class TourFinder
   private int[][] _board;
   private int _sideLength; //board has dimensions n x n
   private boolean _solved = false;
+
+  // ADDED: two arrays for x moves, y moves (matching indices correspond)
+  private static final int[] moveX = {1, 2, 2, 1, -1, -2, -2, -1};
+  private static final int[] moveY = {-2, -1, 1, 2, 2, 1, -1, -2};
 
   //constructor -- build board of size n x n
   public TourFinder( int n )
@@ -119,7 +139,7 @@ class TourFinder
   public String toString()
   {
     //send ANSI code "ESC[0;0H" to place cursor in upper left
-    String retStr = "[0;0H";
+    String retStr = "0H";
     //emacs shortcut: C-q, then press ESC
     //emacs shortcut: M-x quoted-insert, then press ESC
 
@@ -178,6 +198,7 @@ class TourFinder
 
       //mark current cell with current move number
       _board[x][y] = moves;
+      //delay(500);
 
       System.out.println( this ); //refresh screen
 
@@ -193,14 +214,20 @@ class TourFinder
        *     . h . a .
       ******************************************/
       //???
+      for (int i=0; i<8; i++) {
+        int newX = x + moveX[i];
+        int newY = y + moveY[i];
+        findTour(newX, newY, moves+1);
+      }
 
-      
 
       //
 
       //If made it this far, path did not lead to tour, so back up...
       // (Overwrite number at this cell with a 0.)
-      //  ???
+      _board[x][y] = 0;
+      //delay(125);
+      //return;
 
       System.out.println( this ); //refresh screen
     }
