b5b42184eb479635c979a645714459c7bd70fa7e
Julia Kozak <jkozak30@stuy.edu>
2022-03-07 14:49:32 -0500

    kngi

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -1,3 +1,25 @@
+// Great, Nice Jingles: Julia Kozak, Gloria Lee, Nafiz Labib (Flopsy, Flounder, Martha)
+// APCS
+// HW71 -- Whut Dis.
+// 2022-03-08
+// time spent: 0.5hrs
+
+/*
+DISCO:
+ - Step-by-step tracing is a good way of tracking steps.
+ - 
+
+QCC:
+ - 
+
+q0: What does it do?
+ - This program partitions the set into two sets of terms based on the value of index c, and in a given range of indices [a, b]. It checks if each value in that range is less than arr[c], moves it to the front if so, then swaps the value of arr[c] to the end of those.
+
+q1: What is its Big-Oh runtime?
+ - O(n), because it iterates through each value in the range [a, b] and does one action (swap/not swap) in each of those iterations.
+
+*/
+
 public class Mysterion {
 
   public static int[] mysterion(int[] arr, int a, int b, int c) {
@@ -18,7 +40,7 @@ public class Mysterion {
     //
     int temp = arr[b];
     arr[b] = arr[s];
-    arr[s] = arr[b];
+    arr[s] = temp;
 
     return arr;
   }
