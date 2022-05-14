147fd2766e10feb4724f191766c29601f5dadfc1
Maxwell Ito <jules351w@gmail.com>
2022-05-14 19:06:45 -0400

    750005907090540004705070320002503050210001204060

--- 99_alpq/ArrayPriorityQueue.java
+++ 99_alpq/ArrayPriorityQueue.java
@@ -4,22 +4,26 @@ public class ArrayPriorityQueue implements PriorityQueue {
 
   private ArrayList<int[]> _queue;
 
+
+  // constructor
   public ArrayPriorityQueue() {
     _queue = new ArrayList<>();
   }
 
-
-  public void add( int obj, int x ) {
+  // add: O(1)
+  public void add( int obj, int priority ) {
     int[] ret = new int[2];
     ret[0] = obj;
-    ret[1] = x;
+    ret[1] = priority;
     _queue.add(ret);
   }
 
+  // isEmpty: O(1)
   public boolean isEmpty() {
     return _queue.size() <= 0;
   }
 
+  // peekMin: O(n)
   public int peekMin() {
     int min = _queue.get(0)[1];
     int minIndex = 0;
@@ -32,6 +36,8 @@ public class ArrayPriorityQueue implements PriorityQueue {
     return _queue.get(minIndex)[0];
   }
 
+
+  // removeMin: O(n)
   public int removeMin() {
     int min = _queue.get(0)[1];
     int minIndex = 0;
@@ -49,5 +55,16 @@ public class ArrayPriorityQueue implements PriorityQueue {
   public static void main(String[] args) {
     ArrayPriorityQueue a = new ArrayPriorityQueue();
     a.add(7, 1);
+    a.add(36, 2);
+    a.add(1, 5);
+    a.add(55, 4);
+    System.out.println(a.peekMin());
+    System.out.println(a.removeMin());
+    System.out.println(a.peekMin());
+    System.out.println(a.isEmpty());
+    System.out.println(a.removeMin());
+    System.out.println(a.removeMin());
+    System.out.println(a.removeMin());
+    System.out.println(a.isEmpty());
   }
 }
